ce88e33e678899dc304a4091254f3bb9a90f3b12
Rustam <rustam98137@gmail.com>
2026-03-10 20:38:13 +0530

    Return status codes from kernel syscall dispatcher

--- src/syscall.c
+++ src/syscall.c
@@ -2,24 +2,29 @@
 #include "io.h"
 #include "task.h"
 
-void syscall_dispatch(uint64_t syscall_num, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
-    (void)arg3; // Suppress unused parameter warning
+uint64_t syscall_dispatch(uint64_t syscall_num, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
+    (void)arg3;
     switch (syscall_num) {
-        case 0: // read
-            if (arg1 == 0) { // stdin
+        case 0:
+            if (arg1 == 0 && arg2 != 0) {
                 char c;
                 while ((inb(0x3F8 + 5) & 1) == 0);
                 c = inb(0x3F8);
                 *(char *)arg2 = c;
+                return 1;
             }
             break;
-        case 1: // write
-            if (arg1 == 1) { // stdout
+            return (uint64_t)-1;
+        case 1:
+            if (arg1 == 1) {
                 serial_putc((char)arg2);
+                return 1;
             }
             break;
-        case 2: // yield
+            return (uint64_t)-1;
+        case 2:
             schedule();
-            break;
+            return 0;
     }
+    return (uint64_t)-1;
 }
